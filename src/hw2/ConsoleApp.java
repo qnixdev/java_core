ab54e9d825ecc343f30c5241e2c9ca3b9fbc252a
qnixdev <qnixdev@gmail.com>
2022-03-25 20:02:56 +0200

    fix

--- src/hw2/ConsoleApp.java
+++ src/hw2/ConsoleApp.java
@@ -3,48 +3,18 @@ package hw2;
 import java.util.Scanner;
 
 public class ConsoleApp {
-    private static final String hashtag = "#";
-
     public static void main(String[] args) {
-        calculateSum();
-        System.out.println();
-
-        computeDeposit();
-        System.out.println();
-    }
-
-    private static void calculateSum() {
+        String reportText = "the amount of the deposit will be:";
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println(hashtag.repeat(30));
-        System.out.println("# Compute sum of two number. #");
-        System.out.println(hashtag.repeat(30));
+        System.out.println("** Compute your deposit. **");
 
-        System.out.println("Enter first number:");
-        int firstNumber  = scanner.nextInt();
-        System.out.println("Enter second number:");
-        int secondNumber = scanner.nextInt();
-
-        System.out.println(
-            "Sum of this two number are: "
-            + (firstNumber + secondNumber)
-            + ";"
-        );
-
-        System.out.println(hashtag.repeat(30));
-    }
-
-    private static void computeDeposit() {
-        final String reportText = "the amount of the deposit will be:";
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println(hashtag.repeat(30));
-        System.out.println("# Compute your deposit.      #");
-        System.out.println(hashtag.repeat(30));
         System.out.println("Enter your first name:");
         String firstName = scanner.nextLine();
+
         System.out.println("Enter your last name:");
         String lastName = scanner.nextLine();
+
         System.out.println("Enter the deposit amount:");
         double depositAmount = scanner.nextDouble();
 
@@ -65,6 +35,8 @@ public class ConsoleApp {
             + reportText
             + computeDeposit(depositAmount, 10)
         );
+
+        System.out.println();
     }
 
     private static String capitalize(String str) {
@@ -76,7 +48,7 @@ public class ConsoleApp {
         int numberOfYears
     ) {
         final int countDays = numberOfYears * 365;
-        final double percentByDay = (double) 17 / 100 / 365;
+        final double percentByDay = 0.17 / 365;
         depositAmount += (depositAmount * percentByDay) * countDays;
 
         return depositAmount;
