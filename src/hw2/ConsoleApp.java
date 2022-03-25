3ddb1d3150d7706781b2bf66c669500f785e7f37
qnixdev <qnixdev@gmail.com>
2022-03-25 19:47:37 +0200

    fix

--- src/hw2/ConsoleApp.java
+++ src/hw2/ConsoleApp.java
@@ -1,42 +1,41 @@
 package hw2;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ConsoleApp {
     private static final String hashtag = "#";
-    private static final int percent = 17;
 
     public static void main(String[] args) {
-        Worker worker = new Worker();
-
-        calculateSum(worker);
+        calculateSum();
         System.out.println();
 
-        computeDeposit(worker);
+        computeDeposit();
         System.out.println();
     }
 
-    private static void calculateSum(Worker worker) {
+    private static void calculateSum() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println(hashtag.repeat(30));
         System.out.println("# Compute sum of two number. #");
         System.out.println(hashtag.repeat(30));
 
-        int firstNumber  = parseInteger(scanner, "Enter first number");
-        int secondNumber = parseInteger(scanner, "Enter second number");
+        System.out.println("Enter first number:");
+        int firstNumber  = scanner.nextInt();
+        System.out.println("Enter second number:");
+        int secondNumber = scanner.nextInt();
 
         System.out.println(
             "Sum of this two number are: "
-            + worker.sumOfTwoNumber(firstNumber, secondNumber)
+            + (firstNumber + secondNumber)
             + ";"
         );
 
         System.out.println(hashtag.repeat(30));
     }
 
-    private static void computeDeposit(Worker worker) {
+    private static void computeDeposit() {
+        final String reportText = "the amount of the deposit will be:";
         Scanner scanner = new Scanner(System.in);
 
         System.out.println(hashtag.repeat(30));
@@ -46,41 +45,40 @@ public class ConsoleApp {
         String firstName = scanner.nextLine();
         System.out.println("Enter your last name:");
         String lastName = scanner.nextLine();
-        int depositAmount = parseInteger(scanner, "Enter the deposit amount");
+        System.out.println("Enter the deposit amount:");
+        double depositAmount = scanner.nextDouble();
 
         System.out.print("So, " + capitalize(firstName) + ' ' + capitalize(lastName) + ", ");
         System.out.println("your deposit amount: " + depositAmount + '.');
         System.out.println(
-            "For one year, the amount of the deposit will be: "
-            + worker.computeDeposit(depositAmount, 1, percent)
+            "For one year, "
+            + reportText
+            + computeDeposit(depositAmount, 1)
         );
         System.out.println(
-            "For five year, the amount of the deposit will be: "
-            + worker.computeDeposit(depositAmount, 5, percent)
+            "For five year, "
+            + reportText
+            + computeDeposit(depositAmount, 5)
         );
         System.out.println(
-            "For ten years, the amount of the deposit will be: "
-            + worker.computeDeposit(depositAmount, 10, percent)
+            "For ten years, "
+            + reportText
+            + computeDeposit(depositAmount, 10)
         );
-    }
-
-    private static int parseInteger(
-        Scanner scanner,
-        String inputText
-    ) {
-        while (true) {
-            System.out.println(inputText + ":");
-
-            try {
-                return scanner.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("Please enter a valid integer number.");
-                scanner.nextLine();
-            }
-        }
     }
 
     private static String capitalize(String str) {
         return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+
+    private static double computeDeposit(
+        double depositAmount,
+        int numberOfYears
+    ) {
+        final int countDays = numberOfYears * 365;
+        final double percentByDay = (double) 17 / 100 / 365;
+        depositAmount += (depositAmount * percentByDay) * countDays;
+
+        return depositAmount;
     }
 }
